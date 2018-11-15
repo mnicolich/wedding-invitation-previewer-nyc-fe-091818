64e0586556044f72d3de8fe7ca57807c7b155918
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-11-15 14:38:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+// HTML
+// <input> tags 
+// <buttons> for the style (formal, spring, art deco) selections or <nav> or <input type="radio">
+// <div> for the right-hand side 
+
+
+// CSS 
+//  div's height and width must be higher than the form 
+// CSS grid - 2 column grid (left for From, right for invite), one row for the header OR can use Flexbox
+// background - color 
+// background - image 
+// class or ID to switch between the options
+
+// JS 
+// 1. form
+// event listener for typing 
+
+// 2. The styling (form, spring, art deco)
+// onClick on button/div/whater, the style changes the invitation 
+
+
